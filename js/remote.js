c6e2d9efe19a30529cae3a81de784e456477e112
kero-784 <170969138+kero-784@users.noreply.github.com>
2026-08-16 05:49:13 -0700

    Update signaling socket URL and parameters

--- js/remote.js
+++ js/remote.js
@@ -19,7 +19,7 @@ getElement('session-name').innerText = `Session: ${deviceName}`;
 
 // System setup
 const videoElement = getElement('remote-video');
-const signaling = new SignalingSocket('wss://mock-server.local/ws');
+const signaling = new SignalingSocket('wss://629f545e953160.lhr.life', 'controller', deviceId);
 const webrtc = new WebRTCConnection(signaling);
 const inputMgr = new InputManager(videoElement, webrtc);
 
